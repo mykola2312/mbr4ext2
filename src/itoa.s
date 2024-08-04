f86a560f3e90acb9382f7131bb147966870b1d05
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-08-04 18:22:24 +0300

    begin working on itoa

---
+++ src/itoa.s
@@ -0,0 +1,45 @@
+.section .text
+.code16
+
+.globl itoa
+
+.equ    ITOA_BUFFER_SIZE,   12
+
+# eax - number
+# ecx - base
+itoa:
+    push %bx
+    mov $numbers, %bx
+
+.debug:
+    jmp .debug
+
+    mov $(itoa_result + ITOA_BUFFER_SIZE - 1), %di
+    std
+.div:
+    divl %ecx
+    or %eax, %eax
+    jz .end         # we run out of numbers
+
+    # yep, we're using LUT for number -> character conversion since ASCII is a fuck
+    push %eax
+    add %bx, %ax
+    stosb
+    pop %eax
+
+    jmp .div
+.end:
+    cld
+
+    # we return ptr to string, since we're pushing chars in reverse, therefore
+    # beginning of string will change
+    mov %di, %ax
+
+    pop %bx
+    ret
+
+.section .bss
+.comm       itoa_result,        ITOA_BUFFER_SIZE
+
+.section .rodata
+numbers:    .ascii "0123456789ABCDEF"
