9f96ebdfe256f0d21af679b3afe1c5783d54d28b
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-08-07 06:49:49 +0300

    move disk enumeration to its own file for the sake of modularity

--- src/mbr_test.s
+++ src/mbr_test.s
@@ -36,34 +36,6 @@ entry:
 .bootloader:
     call serial_init
 
-    # enumerate all system disks
-    movw $127, disk_id
-    #mov $128, %dl # already set bit 7 to 1 for fixed disk type
-.disk_enum:
-    # set disk id in DL with fixed disk bit on
-    movw disk_id, %dx
-    or $128, %dx
-
-    mov $0x15, %ah
-    int $0x13
-
-    cmp $3, %ah # is fixed disk
-    jnz .disk_skip
-
-    # it's working fixed disk, lets print its number
-    movw disk_id, %ax
-    mov $disk_enum_msg, %si
-    call prints_number
-.disk_skip:
-    decw disk_id
-    jns .disk_enum
-
-    nop
+    call test_disk_enum
 .halt:
     jmp .halt
-
-.section .rodata
-disk_enum_msg:  .asciz  "Disk "
-
-.section .bss
-disk_id:        .word
