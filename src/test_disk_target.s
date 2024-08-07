b8b49b5ce67718b6f080020b0c07a064cb649fda
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-08-07 09:11:33 +0300

    add target disk params

---
+++ src/test_disk_target.s
@@ -0,0 +1,8 @@
+# template file for disk parameters
+# this will target disk 1
+
+.section .rodata
+disk_id:        .word 1
+disk_heads:     .word 16
+disk_cylinders: .word 519
+disk_sectors:   .word 64
